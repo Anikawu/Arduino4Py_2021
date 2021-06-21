9955467ff3e79006275fba45523e6b494095c5e0
Anika <bbvc331@gmail.com>
2021-06-21 19:25:25 +0800

    pySerial control to arduino

--- case01/pySerial_Control_To_Arduino.py
+++ case01/pySerial_Control_To_Arduino.py
@@ -1,7 +1,9 @@
 import serial  #引用pyserial 模組
 import random
 import time
-COM_PORT='COM3'  #指定通訊埠
+#COM_PORT='COM3'  #指定通訊埠
+COM_PORT='COM6'  #指定通訊埠
+#COM_PORT='COM7'  #指定通訊埠
 BAUD_RATES =9600 #設定傳輸速率
 ser = None
 
