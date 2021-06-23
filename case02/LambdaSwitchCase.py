e843c8de245678fe0ca3497d86e3d778bf86cbc6
Anika <bbvc331@gmail.com>
2021-06-23 11:06:36 +0800

    lambda switch

--- case02/LambdaSwitchCase.py
+++ case02/LambdaSwitchCase.py
@@ -10,4 +10,11 @@ sex_info ={
     2:lambda: print("女")
 }
 sex_error = lambda : print("性別錯誤")
-sex_info.get(int(sex),sex_error)()
+sex_info.get(int(sex),sex_error)()  #呼叫lambda的方法
+
+salary_weight ={
+    1:lambda salary:print ("男", salary),
+    2:lambda salary:print ("女",salary*1.2)
+}
+sex_error=lambda:print("性別錯誤")
+salary_weight.get(int(sex),sex_error)(30000)
