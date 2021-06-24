a1a0cf7213e13980e2b6d3ad27b4b43593b8f1b1
Anika <bbvc331@gmail.com>
2021-06-24 15:15:02 +0800

    python

--- case05/TWII.py
+++ case05/TWII.py
@@ -1,31 +1,42 @@
 import requests
+import sqlite3
 
-
-path='https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20210623&selectType=ALL'
+path = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20210623&selectType=ALL'
 csv = requests.get(path).text
-
-#"證券代號","證券名稱","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季"
-
-csv = csv.replace('"1,','"1') #整理資料->先把雙引號 去除
-csv = csv.replace('"2,','"2') #整理資料->先把雙引號 去除
-csv = csv.replace('"3,','"3') #整理資料->先把雙引號 去除
-csv = csv.replace('"','') #整理資料->先把雙引號 去除
-csv=csv.replace('-','-1') #-號 變成-1
-rows=csv.split("\r\n") #\r\n標準斷行
-#"1,040.00"  將雙引號裡面的逗號拿掉
-print(len(rows))  #查詢有幾列資料
-
-stocks=[] #建立一個陣列
+# "證券代號","證券名稱","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
+csv = csv.replace('"1,', '"1')
+csv = csv.replace('"2,', '"2')
+csv = csv.replace('"3,', '"3')
+csv = csv.replace('"', '')
+csv = csv.replace('-', '-1')
+rows = csv.split("\r\n")
+# "1,040.00" 1760
+print(len(rows))
+stocks = []
 for row in rows:
     list = row.split(',')
-    if len(list)==8 and list[0] !='證券代號':
+    if len(list) == 8 and list[0] != '證券代號':
         #print(list)
-        list[2] = float(list[2]) #殖利率(%)
-        list[3] = int(list[3]) #股利年度
-        list[4] = float(list[4]) #本益比
-        list[5] = float(list[5]) #股價淨值比
-        list[7] = '2021-06-23'  # 日期
-        stocks.append(tuple(list)) #將list存到陣列
+        list[2] = float(list[2])  # 殖利率(%)
+        list[3] = int(list[3])    # 股利年度
+        list[4] = float(list[4])  # 本益比
+        list[5] = float(list[5])  # 股價淨值比
+        list[7] = '2021-06-23'
+        stocks.append(tuple(list))
+
 print(stocks)
 
-#匯入資料庫
+# 匯入資料庫
+
+sql = '''
+        insert into Stock(證券代號, 證券名稱, 殖利率, 股利年度, 本益比, 股價淨值比, 財報年季, ts) 
+        values(?, ?, ?, ?, ?, ?, ?, ?)
+      '''
+
+conn = sqlite3.connect('twii.db')
+cursor = conn.cursor()
+cursor.executemany(sql, stocks)
+
+conn.commit()
+conn.close()
+print('OK')
