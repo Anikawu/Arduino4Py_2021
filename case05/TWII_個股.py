a6fabb61e5c4714ad73a226717dde095a7e65d60
Anika <bbvc331@gmail.com>
2021-06-24 16:17:11 +0800

    python

--- case05/TWII_個股.py
+++ case05/TWII_個股.py
@@ -1,7 +1,21 @@
 import requests
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta']  #引用中文字體
+
+
 symbol="2330"
 date="20210624"
 path="https://www.twse.com.tw/exchangeReport/BWIBBU?response=csv&date=%s&stockNo=%s" % (date,symbol)
 
-csv =requests.get(path)
-print(csv.text)
+csv =requests.get(path).text
+#print(csv)
+#"日期","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
+data=csv.split('\r\n')
+data =list(filter(lambda l:len(l.split(','))==7, data ))
+#print(len(data))
+data ="\n".join(data)
+df =pd.read_csv(StringIO(data)) #將列表丟到csv裡面
+print(data)
+print(df.dtypes) #列出每個資料欄位的型態
