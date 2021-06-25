8c3915f364c644f4944b7a27765f4b2b596019ed
Anika <bbvc331@gmail.com>
2021-06-25 17:08:21 +0800

    python

--- case01/Arduino_GUI.py
+++ case01/Arduino_GUI.py
@@ -15,13 +15,14 @@ from PIL import Image, ImageTk
 COM_PORT = 'COM8'  # 指定通訊埠名稱
 BAUD_RATES = 9600  # 設定傳輸速率(鮑率)
 play = True
-
+data=""
 def receiveData():
 
     while play:
         try:
             global ser
             data_row = ser.readline()  # 讀取一行(含換行符號\r\n)原始資料
+            global data
             data = data_row.decode()  # 預設是用 UTF-8 解碼
             data = data.strip("\r").strip("\n")  # 除去換行符號
             print(data)
@@ -31,6 +32,20 @@ def receiveData():
                 cdsValue.set("%d lu" % (float(values[0])))
                 tempValue.set("%.1f C" % (float(values[1])))
                 humiValue.set("%.1f %%" % (float(values[2])))
+                if(int(values[3]) == 16):
+                    sendButton0.config(image=buzeer_open_photo)
+                    sendButton0.image = buzeer_open_photo
+                elif (int(values[3]) == 32):
+                    sendButton0.config(image=buzeer_close_photo)
+                    sendButton0.image = buzeer_close_photo
+
+                if (int(values[4]) == 65): #65:關門狀態
+                    sendButton4.config(image=door_close_photo)
+                    sendButton4.image = door_close_photo
+                elif (int(values[4]) == 165):
+
+                    sendButton4.config(image=door_open_photo)
+                    sendButton4.image = door_open_photo
             except:
                 pass
 
@@ -45,30 +60,39 @@ def receiveData():
                 print("Serial exception: ", e)
 
             #break
-
 def sendData(n):
     data_row = n + "#" # "#" 代表結束符號
     data = data_row.encode()
     ser.write(data)
     print("send: ", data_row, data)
 
+def openTheDoor():
+    door = int(data.split(",")[4])
+    if door == 165 :
+        sendData('8')
+    elif door == 65:
+        sendData('4')
+
 def getOpenWeatherData():
     status_code, main, icon, temp, feels_like, humidity = ow.openweather()
     if(status_code == 200):
         owmainValue.set(main)
-        #取的 ICON 圖片 BYTES資料
+
+        # 取得 icon 圖片 bytes 資料
         raw_data = ow.openweatherIcon(icon)
-        #轉成 image 格式
+        # 轉成 image 格式
         im = Image.open(BytesIO(raw_data))
-        #轉成 TK 的photo 格式
+        # 轉成 Tk 的 photo 格式
         photo = ImageTk.PhotoImage(im)
-        #配置到目標區(owiconLabel)
+        # 配置到目標區(owiconLabel)
         owiconLabel.config(image=photo)
         owiconLabel.image=photo
 
-        owtempValue.set(temp)
-        owfeelsLikeValue.set(feels_like)
-        owhumidityValue.set(humidity)
+        owtempValue.set("%.1f C" % (float(temp)-273.15))
+        owfeelsLikeValue.set("%.1f C" % (float(feels_like)-273.15))
+        owhumidityValue.set("%.1f %%" % float(humidity))
+
+        sendData("A%.2f,%.2f" % ((float(temp)-273.15), float(humidity)))
     else:
         owmainValue.set('錯誤碼：' + str(status_code))
 
@@ -81,10 +105,19 @@ if __name__ == '__main__':
 
     root = tkinter.Tk()
     root.geometry("800x400")
-    root.title("Arduino GUI")
+    root.title("Arduino 智慧家庭")
 
     myfont1 = font.Font(family='Helvetica', size=36, weight='bold')
     myfont2 = font.Font(family='Helvetica', size=24)
+
+    buzeer_open_photo = ImageTk.PhotoImage(Image.open('buzeer_open.png'))
+    buzeer_close_photo = ImageTk.PhotoImage(Image.open('buzeer_close.png'))
+    clear_photo = ImageTk.PhotoImage(Image.open('clear.png'))
+    red_photo = ImageTk.PhotoImage(Image.open('red.png'))
+    green_photo = ImageTk.PhotoImage(Image.open('green.png'))
+    yellow_photo = ImageTk.PhotoImage(Image.open('yellow.png'))
+    door_open_photo = ImageTk.PhotoImage(Image.open('door_open.png'))
+    door_close_photo = ImageTk.PhotoImage(Image.open('door_close.png'))
 
     # 爬蟲 Openweather -----------------------------------------------------------------
     owmainValue = tkinter.StringVar()
@@ -111,12 +144,11 @@ if __name__ == '__main__':
     respText = tkinter.StringVar()
     respText.set("0,0.0,0.0")
 
-    sendButton0  = tkinter.Button(text='0', command=lambda: sendData('0'), font=myfont2)
-    sendButton1  = tkinter.Button(text='1', command=lambda: sendData('1'), font=myfont2)
-    sendButton2  = tkinter.Button(text='2', command=lambda: sendData('2'), font=myfont2)
-    sendButton3  = tkinter.Button(text='3', command=lambda: sendData('3'), font=myfont2)
-    sendButton4  = tkinter.Button(text='4', command=lambda: sendData('4'), font=myfont2)
-    sendButton8  = tkinter.Button(text='8', command=lambda: sendData('8'), font=myfont2)
+    sendButton0  = tkinter.Button(text='16', image=buzeer_close_photo, command=lambda: sendData('16'), font=myfont2)
+    sendButton1  = tkinter.Button(text='1', image=red_photo, command=lambda: sendData('1'), font=myfont2)
+    sendButton2  = tkinter.Button(text='2', image=green_photo, command=lambda: sendData('2'), font=myfont2)
+    sendButton3  = tkinter.Button(text='3', image=yellow_photo, command=lambda: sendData('3'), font=myfont2)
+    sendButton4  = tkinter.Button(text='4', image=door_close_photo, command=lambda: openTheDoor(), font=myfont2)
     # 爬蟲 Openweather -----------------------------------------------------------------
     owmainButton = tkinter.Button(textvariable=owmainValue, command=lambda: getOpenWeatherData(), font=myfont2)
     owiconLabel = tkinter.Label(root, image=None)
@@ -136,8 +168,7 @@ if __name__ == '__main__':
     sendButton1.grid(row=0,   column=1, columnspan=1, sticky='EWNS')
     sendButton2.grid(row=0,   column=2, columnspan=1, sticky='EWNS')
     sendButton3.grid(row=0,   column=3, columnspan=1, sticky='EWNS')
-    sendButton4.grid(row=0,   column=4, columnspan=1, sticky='EWNS')
-    sendButton8.grid(row=0,   column=5, columnspan=1, sticky='EWNS')
+    sendButton4.grid(row=0,   column=4, columnspan=2, sticky='EWNS')
     # 爬蟲 Openweather -----------------------------------------------------------------
     owiconLabel.grid(row=1, column=0, columnspan=1, sticky='EWNS')
     owmainButton.grid(row=1, column=1, columnspan=2, sticky='EWNS')
@@ -148,9 +179,13 @@ if __name__ == '__main__':
     cdsLabel.grid(row=2,  column=0, columnspan=2, sticky='EWNS')
     tempLabel.grid(row=2, column=2, columnspan=2, sticky='EWNS')
     humiLabel.grid(row=2, column=4, columnspan=2, sticky='EWNS')
+    # ----------------------------------------------------------------------------------
     receiveLabel.grid(row=3,  column=0, columnspan=6, sticky='EWNS')
 
     t1 = threading.Thread(target=receiveData)
     t1.start()
+
+    t2 = threading.Thread(target=getOpenWeatherData)
+    t2.start()
 
     root.mainloop()
